02d619167f68140e2a34ddb7620f081764fcd513
James Oldfield <hi@james-oldfield.co>
2015-12-08 22:01:52 +0000

    updating to stateless componenets + airbnb's eslintrc

--- src/components/react-rpg.jsx
+++ src/components/react-rpg.jsx
@@ -1,32 +1,30 @@
-import React from "react";
-import ReactRpgPhoto from "./react-rpg-photo";
+import React from 'react';
+import ReactRpgPhoto from './react-rpg-photo';
 
-export default class ReactRpg extends React.Component {
-  render() {
-    const { imagesArray, padding } = this.props;
-    const columns = this.props.columns || 3;
-    const width = Math.floor(100 / columns);
+const ReactRpg = ({ imagesArray, padding, columns = 3 }) => {
+  const width = Math.floor(100 / columns);
 
-    const imageNodes = imagesArray.map((arr, index) => {
-      return (
-        <ReactRpgPhoto key={index} url={arr.url} link={arr.link} width={width} padding={padding}/>
-      );
-    });
-
+  const imageNodes = imagesArray.map((arr, index) => {
     return (
-      <div className="imageGrid">
-        {imageNodes}
-      </div>
+      <ReactRpgPhoto key={index} url={arr.url} link={arr.link} width={width} padding={padding}/>
     );
-  }
-}
+  });
+
+  return (
+    <div className="imageGrid">
+      {imageNodes}
+    </div>
+  );
+};
 
 ReactRpg.propTypes = {
   imagesArray: React.PropTypes.array.isRequired,
   columns: React.PropTypes.number,
-  padding: React.PropTypes.number
+  padding: React.PropTypes.number,
 };
 
 ReactRpg.defaultProps = {
-  imagesArray: []
+  imagesArray: [],
 };
+
+export default ReactRpg;
