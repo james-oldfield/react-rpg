e9a59c69c7799503cc95269525800b75e1ccf223
James Oldfield <hi@james-oldfield.co>
2015-12-08 22:14:44 +0000

    pushing minified build files

--- dist/react-rpg.js
+++ dist/react-rpg.js
@@ -64,21 +64,13 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
-	Object.defineProperty(exports, "__esModule", {
+	Object.defineProperty(exports, '__esModule', {
 	  value: true
 	});
 	
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-	
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
 	var _react = __webpack_require__(2);
 	
@@ -88,52 +80,37 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var _reactRpgPhoto2 = _interopRequireDefault(_reactRpgPhoto);
 	
-	var ReactRpg = (function (_React$Component) {
-	  _inherits(ReactRpg, _React$Component);
+	var ReactRpg = function ReactRpg(_ref) {
+	  var imagesArray = _ref.imagesArray;
+	  var padding = _ref.padding;
+	  var _ref$columns = _ref.columns;
+	  var columns = _ref$columns === undefined ? 3 : _ref$columns;
 	
-	  function ReactRpg() {
-	    _classCallCheck(this, ReactRpg);
+	  var width = Math.floor(100 / columns);
 	
-	    _get(Object.getPrototypeOf(ReactRpg.prototype), "constructor", this).apply(this, arguments);
-	  }
+	  var imageNodes = imagesArray.map(function (arr, index) {
+	    return _react2['default'].createElement(_reactRpgPhoto2['default'], { key: index, url: arr.url, link: arr.link, width: width, padding: padding });
+	  });
 	
-	  _createClass(ReactRpg, [{
-	    key: "render",
-	    value: function render() {
-	      var _props = this.props;
-	      var imagesArray = _props.imagesArray;
-	      var padding = _props.padding;
-	
-	      var columns = this.props.columns || 3;
-	      var width = Math.floor(100 / columns);
-	
-	      var imageNodes = imagesArray.map(function (arr, index) {
-	        return _react2["default"].createElement(_reactRpgPhoto2["default"], { key: index, url: arr.url, link: arr.link, width: width, padding: padding });
-	      });
-	
-	      return _react2["default"].createElement(
-	        "div",
-	        { className: "imageGrid" },
-	        imageNodes
-	      );
-	    }
-	  }]);
-	
-	  return ReactRpg;
-	})(_react2["default"].Component);
-	
-	exports["default"] = ReactRpg;
+	  return _react2['default'].createElement(
+	    'div',
+	    { className: 'imageGrid' },
+	    imageNodes
+	  );
+	};
 	
 	ReactRpg.propTypes = {
-	  imagesArray: _react2["default"].PropTypes.array.isRequired,
-	  columns: _react2["default"].PropTypes.number,
-	  padding: _react2["default"].PropTypes.number
+	  imagesArray: _react2['default'].PropTypes.array.isRequired,
+	  columns: _react2['default'].PropTypes.number,
+	  padding: _react2['default'].PropTypes.number
 	};
 	
 	ReactRpg.defaultProps = {
 	  imagesArray: []
 	};
-	module.exports = exports["default"];
+	
+	exports['default'] = ReactRpg;
+	module.exports = exports['default'];
 
 /***/ },
 /* 2 */
@@ -145,87 +122,65 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
-	Object.defineProperty(exports, "__esModule", {
+	Object.defineProperty(exports, '__esModule', {
 	  value: true
 	});
 	
-	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-	
-	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
 	var _react = __webpack_require__(2);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var ReactRpgPhoto = (function (_React$Component) {
-	  _inherits(ReactRpgPhoto, _React$Component);
+	var ReactRpgPhoto = function ReactRpgPhoto(_ref) {
+	  var url = _ref.url;
+	  var width = _ref.width;
+	  var padding = _ref.padding;
+	  var _ref$link = _ref.link;
+	  var link = _ref$link === undefined ? url : _ref$link;
+	  return (function () {
+	    var styles = {
+	      imageGridItem: {
+	        display: 'inline-block',
+	        width: width + '%',
+	        boxSizing: 'border-box',
+	        float: 'left',
+	        padding: padding
+	      },
+	      imageWrapper: {
+	        position: 'relative',
+	        width: '100%',
+	        paddingBottom: '100%',
+	        backgroundImage: 'url(' + url + ')',
+	        backgroundSize: 'cover',
+	        backgroundPosition: 'center center',
+	        backgroundRepeat: 'no-repeat'
+	      }
+	    };
 	
-	  function ReactRpgPhoto() {
-	    _classCallCheck(this, ReactRpgPhoto);
-	
-	    _get(Object.getPrototypeOf(ReactRpgPhoto.prototype), "constructor", this).apply(this, arguments);
-	  }
-	
-	  _createClass(ReactRpgPhoto, [{
-	    key: "render",
-	    value: function render() {
-	      var _props = this.props;
-	      var url = _props.url;
-	      var width = _props.width;
-	      var padding = _props.padding;
-	
-	      var link = this.props.link || url;
-	
-	      var styles = {
-	        imageGridItem: {
-	          display: "inline-block",
-	          width: width + "%",
-	          boxSizing: "border-box",
-	          padding: padding
-	        },
-	        imageWrapper: {
-	          position: "relative",
-	          width: "100%",
-	          paddingBottom: "100%",
-	          backgroundImage: "url(" + url + ")",
-	          backgroundSize: "cover",
-	          backgroundPosition: "center center",
-	          backgroundRepeat: "no-repeat"
-	        }
-	      };
-	
-	      return _react2["default"].createElement(
-	        "div",
-	        { className: "imageGridItem", style: styles.imageGridItem },
-	        _react2["default"].createElement(
-	          "a",
-	          { href: link },
-	          _react2["default"].createElement("div", { className: "imageWrapper", style: styles.imageWrapper })
-	        )
-	      );
-	    }
-	  }]);
-	
-	  return ReactRpgPhoto;
-	})(_react2["default"].Component);
-	
-	exports["default"] = ReactRpgPhoto;
+	    return _react2['default'].createElement(
+	      'div',
+	      { className: 'imageGridItem', style: styles.imageGridItem },
+	      _react2['default'].createElement(
+	        'a',
+	        { href: link },
+	        _react2['default'].createElement('div', { className: 'imageWrapper', style: styles.imageWrapper })
+	      )
+	    );
+	  })();
+	};
 	
 	ReactRpgPhoto.propTypes = {
-	  url: _react2["default"].PropTypes.string.isRequired,
-	  width: _react2["default"].PropTypes.number.isRequired,
-	  padding: _react2["default"].PropTypes.number,
-	  link: _react2["default"].PropTypes.string
+	  url: _react2['default'].PropTypes.string.isRequired,
+	  width: _react2['default'].PropTypes.number.isRequired,
+	  padding: _react2['default'].PropTypes.number,
+	  link: _react2['default'].PropTypes.string
 	};
-	module.exports = exports["default"];
+	
+	exports['default'] = ReactRpgPhoto;
+	module.exports = exports['default'];
 
 /***/ }
 /******/ ])
